126576324183fcdd6dd63d729a13c94475d80ead
prianka02 <priankaakter115@gmail.com>
2024-09-27 16:03:19 +0600

    Navigation Drawer has been added successfully

--- app/src/main/java/com/ecommapp/marketino/ui/home/HomeFragment.kt
+++ app/src/main/java/com/ecommapp/marketino/ui/home/HomeFragment.kt
@@ -8,22 +8,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.ecommapp.marketino.R
 import com.ecommapp.marketino.data.products.Product
 import com.ecommapp.marketino.databinding.FragmentHomeBinding
+import com.ecommapp.marketino.databinding.NavViewHeaderBinding
 import kotlinx.coroutines.launch
 
 class HomeFragment : Fragment() {
     private lateinit var adapter: ProductAdapter
     private lateinit var viewModel: HomeViewModel
     private lateinit var homeBinding: FragmentHomeBinding
+    private lateinit var navViewHeaderBinding: NavViewHeaderBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -35,10 +34,41 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+//        Attach ViewBinding
         homeBinding = FragmentHomeBinding.bind(view)
+        navViewHeaderBinding = NavViewHeaderBinding.bind(homeBinding.navView.getHeaderView(0))
 
+//        Add ViewModel Provider
         viewModel = ViewModelProvider(this)[HomeViewModel::class.java]
 
+
+        //  Handle Navigation icon Click
+        homeBinding.topAppBar.setOnClickListener {
+//            Open's the Navigation Drawer
+            homeBinding.drawerLayout.openDrawer(homeBinding.navView)
+        }
+
+//        Top Bar items Selection
+        homeBinding.topAppBar.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.search -> {
+                    // Handle edit text press
+                    Toast.makeText(requireContext(), "Search Clicked", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.more -> {
+                    // Handle more item (inside overflow menu) press
+                    Toast.makeText(requireContext(), "More Clicked", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
+        }
+        navViewHeaderBinding.logoutBtn.setOnClickListener {
+            Toast.makeText(requireContext(), "Logout Clicked", Toast.LENGTH_SHORT).show()
+        }
+
+//        Recycler View Handling
         homeBinding.recyclerView.layoutManager = GridLayoutManager(requireContext(), 2)
         homeBinding.recyclerView.addItemDecoration(ItemSpacingDecoration(horizontal = 2, vertical = 2))
         homeBinding.recyclerView.setPadding(8, 0, 0, 0)
@@ -46,6 +76,7 @@ class HomeFragment : Fragment() {
 //        Handle the Loading
         handleLoading()
 
+//        get Product Response from viewModel
         lifecycleScope.launch {
             viewModel.productResponseFlow.collect { response ->
                 Log.d("HomeFragment", response.toString())
--- app/src/main/java/com/ecommapp/marketino/ui/home/ShopFragment.kt
+++ app/src/main/java/com/ecommapp/marketino/ui/home/ShopFragment.kt
@@ -6,21 +6,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ProgressBar
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.ecommapp.marketino.R
 import com.ecommapp.marketino.databinding.FragmentShopBinding
+import com.ecommapp.marketino.databinding.NavViewHeaderBinding
 import kotlinx.coroutines.launch
 
 class ShopFragment : Fragment() {
-//    private lateinit var categoryrecyclerView: RecyclerView
     private lateinit var categoryAdapter: CategoryAdapter
-//    private lateinit var progressBar: ProgressBar
     private lateinit var viewModel: ShopViewModel
     private lateinit var categoryBinding: FragmentShopBinding
+    private lateinit var navViewHeaderBinding: NavViewHeaderBinding
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,24 +32,52 @@ class ShopFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
 //        View binding added
         categoryBinding = FragmentShopBinding.bind(view)
+        navViewHeaderBinding = NavViewHeaderBinding.bind(categoryBinding.navView.getHeaderView(0))
+
 
 //        ShopViewModel has added
         viewModel = ViewModelProvider(this)[ShopViewModel::class.java]
 
-//
-//        categoryrecyclerView = view.findViewById(R.id.rvParent)
-//        progressBar = view.findViewById(R.id.progressBar)
 
+        //  Handle Navigation icon Click
+        categoryBinding.topAppBar.setOnClickListener {
+//            Open's the Navigation Drawer
+            categoryBinding.drawerLayout.openDrawer(categoryBinding.navView)
+        }
+
+//        Top Bar items Selection
+        categoryBinding.topAppBar.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.search -> {
+                    // Handle edit text press
+                    Toast.makeText(requireContext(), "Search Clicked", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.more -> {
+                    // Handle more item (inside overflow menu) press
+                    Toast.makeText(requireContext(), "More Clicked", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
+        }
+        navViewHeaderBinding.logoutBtn.setOnClickListener {
+            Toast.makeText(requireContext(), "Logout Clicked", Toast.LENGTH_SHORT).show()
+        }
+
+//        Add layout manager in the recycler view
         categoryBinding.rvParent.layoutManager = LinearLayoutManager(requireContext())
 
+//        Set Empty adapter
         categoryAdapter = CategoryAdapter(emptyList()) { item ->
-
             Log.d("Clicked", item.toString())
 //            navigateToDetails(item)
         }
         categoryBinding.rvParent.adapter = categoryAdapter
+
 //        Handle the Loading
         handleLoading()
 
